39dfbbc5068077a79e0b394bc0f51e0beae762fa
6YuUyuu9 <nraekasin0@gmail.com>
2026-03-28 18:02:25 +0700

    Update userService.js
    เพิ่ม function การค้นหา อัพเดท ลบ แก้ไขรหัสผ่าน

--- Queue-Management-System/src/services/userService.js
+++ Queue-Management-System/src/services/userService.js
@@ -5,12 +5,32 @@ export const userService = {
         const response = await api.post('/user.php/login', { username, password });
         return response.data;
     },
-    register: async (username, password) => {
-        const response = await api.post('/user.php/register', { username, password });
+    register: async (username, password, role = 'user') => {
+        const response = await api.post('/user.php/register', { username, password, role });
         return response.data;
     },
-    getUsers: async () => {
-        const response = await api.get('/user.php/users');
+    getAll: async () => {
+        const response = await api.get('/user.php/list');
+        return response.data;
+    },
+    getById: async (userId) => {
+        const response = await api.get(`/user.php/get?id=${userId}`);
+        return response.data;
+    },
+    update: async (userId, username, role) => {
+        const response = await api.post('/user.php/update', { user_id: userId, username, role });
+        return response.data;
+    },
+    delete: async (userId) => {
+        const response = await api.post('/user.php/delete', { user_id: userId });
+        return response.data;
+    },
+    changePassword: async (userId, oldPassword, newPassword) => {
+        const response = await api.post('/user.php/change-password', { 
+            user_id: userId, 
+            old_password: oldPassword, 
+            new_password: newPassword 
+        });
         return response.data;
     }
 };
