3934daf880ca663b7110eaf59cfa7db18836f39a
Vanna-study <vannapars04@gmail.com>
2026-03-28 14:16:18 +0700

    feat : create page routing

--- Queue-Management-System/src/App.jsx
+++ Queue-Management-System/src/App.jsx
@@ -1,9 +1,22 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Admin from './pages/Admin'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
 
 function App() {
-
   return (
     <>
       App
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
     </>
   )
 }
