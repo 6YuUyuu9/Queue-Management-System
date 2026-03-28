ddec84dcaa329de3840d165270fc86a7e4c5bd62
Vanna-study <vannapars04@gmail.com>
2026-03-28 14:11:19 +0700

    feat: routing setup

---
+++ Queue-Management-System/src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+     <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/signin">Sign In</NavLink>
+      <NavLink to="/signup">Sign Up</NavLink>
+      <NavLink to="/admin">Admin</NavLink>
+    </nav>
+  )
+}
+
+export default Navbar
